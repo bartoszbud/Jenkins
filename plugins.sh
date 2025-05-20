8822a2b5002e1a3d2cca5b2ae462f10b1fa75f6d
Bartosz Budzynowski <bartoszbud@gmail.com>
2025-05-20 22:31:12 +0200

    Update plugins.sh

--- plugins.sh
+++ plugins.sh
@@ -11,7 +11,7 @@ JENKINS_API_TOKEN="jenkins123"
 # Get local plugin versions
 echo "[INFO] Getting list of installed plugins..."
 #plugins=$(curl -s -u "$JENKINS_USER:$JENKINS_API_TOKEN" "$JENKINS_URL/pluginManager/api/json?depth=1" \
-    | jq -r '.plugins[] | "\(.shortName):\(.version)"')
+#    | jq -r '.plugins[] | "\(.shortName):\(.version)"')
 plugins=$(cat ./config/plugins.txt)
 
 echo "[INFO] Persist local plugins to file..."
